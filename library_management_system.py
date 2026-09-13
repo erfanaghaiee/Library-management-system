c425471c7f530217ae42cd04c5a8d4069288c155
erfan aghaee <erfan4erfan6@gmail.com>
2026-09-13 15:53:52 +0330

    add book class and add library class library just has addbook method yet.

--- library_management_system.py
+++ library_management_system.py
@@ -4,4 +4,23 @@ import sqlite3
 connection = sqlite3.connect("books.db")
 cursor = connection.cursor()
 
+class Book:
+    def __init__(self,title , author , year , status = bool):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.status = status
+class Library:
+    def __init__(self):
+        pass
+    def addBook(self , book):
+        quary = "INSERT INTO books (title , author , year , status) VALUES (? , ? , ? , ?)"
+        parametr = (book.title , book.author , book.year , book.status)
+        cursor.execute(quary , parametr)
+        connection.commit()
+
+
+    
+        
+        
 
